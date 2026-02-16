62ef65478b742df7bf750e1e86d0724fee42bfdc
Sergey Skorik <Skorik.SN@phystech.edu>
2026-02-16 09:31:56 +0000

    update stdout redirection

--- src/utils/logging_utils.py
+++ src/utils/logging_utils.py
@@ -8,21 +8,43 @@ def redirect_stdout_to_log():
     # Read output file (created by >output/file.txt)
     redirect_file = subprocess.run(
         ["readlink", "-f", f"/proc/{os.getpid()}/fd/1"], capture_output=True, text=True
-    ).stdout
-    redirect_file = redirect_file[: len(redirect_file) - 1]  # delete \n
-    os.remove(redirect_file)
+    ).stdout.strip()
 
-    # Get file to log learning (in dir created by hydra)
+    # hydra log file
     main_log_file = (
         hydra.core.hydra_config.HydraConfig.get().runtime.output_dir + "/output.txt"
     )
-
-    # Swap stdout to log file
     f = open(main_log_file, "w")
+
+    # If stdout is a terminal -> keep console + also write to file
+    if redirect_file.startswith("/dev/pts/"):
+
+        class Tee:
+            def __init__(self, a, b):
+                self.a, self.b = a, b
+
+            def write(self, s):
+                self.a.write(s)
+                self.b.write(s)
+
+            def flush(self):
+                self.a.flush()
+                self.b.flush()
+
+        sys.stdout = Tee(sys.__stdout__, f)
+        sys.stderr = Tee(sys.__stderr__, f)
+
+        print("Information about files:")
+        print(f"File to logging: {main_log_file}")
+        print()
+        return
+
+    # Otherwise stdout was redirected to a file
+    os.remove(redirect_file)
+
     sys.stdout = f
     sys.stderr = f
 
-    # Create link to log file (output/file.txt link to log file)
     os.symlink(main_log_file, redirect_file)
 
     print("Information about files:")
